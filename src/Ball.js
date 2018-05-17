42acd3668326a500f2494fddcd7cb21fcb830b1e
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-05-18 02:39:03 +0700

    Cleaned up code

--- src/Ball.js
+++ src/Ball.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { View, Animated } from 'react-native';
 
 class Ball extends Component {
   componentWillMount() {
     this.position = new Animated.ValueXY(0, 0);
-    console.log(this.position);
     Animated.spring(this.position, {
-      toValue: { x: 200, y: 500 }
+      toValue: { x: 200, y: 500 },
     }).start();
   }
 
