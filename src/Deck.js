751b9376d6a0753fa37f561009bcb493162b369a
NatashaKSS <NatashaKSS@users.noreply.github.com>
2018-06-23 16:55:48 +0800

    Added explanations of PanResponder callbacks

--- src/Deck.js
+++ src/Deck.js
@@ -10,8 +10,14 @@ class Deck extends Component {
     super(props);
 
     const panResponder = PanResponder.create({
+      // called when user presses on the screen that decides whether this
+      // PanResponder instance should be responsible for this touch event
       onStartShouldSetPanResponder: () => {},
+
+      // called many times as the user drags the draggable around the screen
       onPanResponderMove: () => {},
+
+      // called when user removes touch from screen
       onPanResponderRelease: () => {},
     });
 
